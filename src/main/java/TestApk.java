5167d7c773d6cdadb428b2bff4aebb66537d8360
fta <1018598742@qq.com>
2018-11-07 16:49:57 +0800

    修改了

--- src/main/java/TestApk.java
+++ src/main/java/TestApk.java
@@ -4,9 +4,8 @@ public class TestApk {
 
     public static void main(String[] args) {
         ApkUtils apkUtils = new ApkUtils();
-//        apkUtils.getSha1("C:\\Users\\fta\\Desktop\\app-debug.apk");
-//        apkUtils.getSha1("C:\\Users\\fta\\Desktop\\鼎桥jar和文档\\mobilestyletclient-release.apk");
-        String sha1 = apkUtils.getSha1("C:\\Users\\fta\\Desktop\\mytemp\\alipay_wap_main.apk");
+//        String sha1 = apkUtils.getSha1Other("C:\\Users\\fta\\Desktop\\mytemp\\alipay_wap_main.apk");
+        String sha1 = apkUtils.getSha1Other("C:\\Users\\fta\\Desktop\\mytemp\\app-debug.apk");
         System.out.println("签名是：" + sha1);
     }
 }
--- src/main/java/utils/ApkUtils.java
+++ src/main/java/utils/ApkUtils.java
@@ -1,7 +1,9 @@
 package utils;
 
 import java.io.*;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
 /**
@@ -65,6 +67,90 @@ public class ApkUtils {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        return apkSHA1;
+    }
+
+    /**
+     * @param apkPath apk的绝对路径
+     * @return
+     */
+    public String getSha1Other(String apkPath) {
+        String apkSHA1 = "";
+        File apkFile = new File(apkPath);
+        if (apkFile.exists()) {
+            try {
+                ZipFile zipFile = new ZipFile(apkFile);
+                Enumeration<? extends ZipEntry> files = zipFile.entries();
+                ZipEntry entry = null;
+                String outFilePath = ".//temp//CERT.RSA";
+                File outFile = null;
+                InputStream zipFileInputStream;
+                OutputStream outputStream;
+                while (files.hasMoreElements()) {
+                    entry = files.nextElement();
+                    String entryName = entry.getName();
+                    if (!entryName.contains("CERT.RSA")) {
+                        continue;
+                    }
+                    System.out.println(entryName);
+                    zipFileInputStream = zipFile.getInputStream(entry);
+                    outFile = new File(outFilePath);
+                    if (!outFile.exists()) {
+                        new File(outFile.getParent()).mkdirs();
+                    }
+                    System.out.println("绝对路径：" + outFile.getAbsolutePath());
+                    outputStream = new FileOutputStream(outFile);
+                    int len = 0;
+                    byte[] bytes = new byte[1024];
+                    while ((len = zipFileInputStream.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, len);
+                    }
+                    zipFileInputStream.close();
+                    outputStream.flush();
+                    outputStream.close();
+
+                    apkSHA1 = executeCmd(outFile.getAbsolutePath());
+
+                    break;
+                }
+
+                //删除临时文件
+                if (outFile != null && outFile.exists()) {
+                    if (outFile.isFile()) {
+                        outFile.delete();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return apkSHA1;
+    }
+
+    private String executeCmd(String filePath) throws IOException {
+        String apkSHA1 = "";
+        File signerFile = new File(filePath);
+        if (signerFile.exists()) {
+            //执行命令
+            Process process = Runtime.getRuntime().exec(cmdInfo + filePath);
+            //取得命令结果的输出流
+            InputStream inputStream = process.getInputStream();
+            //用一个读输出流类去读
+            //用缓冲器读行
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "GB2312"));
+            String line = null;
+            //直到读完为止
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+//                    System.out.println("每一行的数据：" + line);
+                if (line.contains("SHA1:")) {
+                    apkSHA1 = line.substring("SHA1: ".length() + 1);
+                    break;
+                }
+            }
+        } else {
+            System.out.println("要解析的文件不存在");
         }
         return apkSHA1;
     }
