6b95bac023a49d3a4e18a2b32b7957d8e66a1ba1
1018598742 <1018598742@qq.com>
2018-11-07 16:49:07 +0800

    Update TestApk.java

--- src/main/java/TestApk.java
+++ src/main/java/TestApk.java
@@ -5,7 +5,6 @@ public class TestApk {
     public static void main(String[] args) {
         ApkUtils apkUtils = new ApkUtils();
 //        apkUtils.getSha1("C:\\Users\\fta\\Desktop\\app-debug.apk");
-//        apkUtils.getSha1("C:\\Users\\fta\\Desktop\\鼎桥jar和文档\\mobilestyletclient-release.apk");
         String sha1 = apkUtils.getSha1("C:\\Users\\fta\\Desktop\\mytemp\\alipay_wap_main.apk");
         System.out.println("签名是：" + sha1);
     }
